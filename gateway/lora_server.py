7332bc9488eb41a7164b050428a498aa9ef051c9
Petr Kucera <petr@khome.cz>
2024-04-16 21:36:40 +0200

    Implement custom library

--- gateway/lora_server.py
+++ gateway/lora_server.py
@@ -1,4 +1,5 @@
-from pyLoraRFM9x import LoRa, ModemConfig
+from custom_lora import LoRa, ModemConfig
+import board
 
 # This is our callback function that runs when a message is received
 def on_recv(payload):
@@ -6,10 +7,22 @@ def on_recv(payload):
     print("Received:", payload.message)
     print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
 
-# Lora object will use spi port 0 and use chip select 1. GPIO pin 5 will be used for interrupts and set reset pin to 25
+# Use chip select 0. GPIO pin 17 will be used for interrupts
 # The address of this device will be set to 2
-lora = LoRa(0, 1, 5, 2, reset_pin = 25, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
+lora = LoRa(
+    channel=0, # channel SPI channel to use (either 0 or 1, if your LoRa radio is connected to CE0 or CE1, respectively)
+    interrupt=25, # GPIO pin (BCM-style numbering) to use for the interrupt
+    this_address=2, # The address number (0-254) your device will use when sending and receiving packets.
+    freq=868, # Frequency used by your LoRa radio. Defaults to 915Mhz
+    tx_power=14, # Transmission power level from 5 to 23. Keep this as low as possible. Defaults to 14.
+    modem_config=ModemConfig.Bw500Cr45Sf128, # Modem configuration. See RadioHead docs. Default to Bw125Cr45Sf128.
+    receive_all=True, # Receive messages regardless of the destination address
+    acks=False, # If True, send an acknowledgment packet when a message is received and wait for an acknowledgment when transmitting a message. This is equivalent to using RadioHead's RHReliableDatagram
+    crypto=None # An instance of PyCryptodome Cipher.AES (see above example)
+    )
 lora.on_recv = on_recv
+
+lora.set_mode_rx()
 
 # Send a message to a recipient device with address 10
 # Retry sending the message twice if we don't get an  acknowledgment from the recipient
@@ -19,3 +32,6 @@ if status is True:
     print("Message sent!")
 else:
     print("No acknowledgment from recipient")
+    
+# And remember to call this as your program exits...
+lora.close()
--- gateway/simpletest.py
+++ gateway/simpletest.py
@@ -16,9 +16,11 @@ RADIO_FREQ_MHZ = 868.0  # Frequency of the radio in Mhz. Must match your
 CS = digitalio.DigitalInOut(board.D25)
 RESET = digitalio.DigitalInOut(board.D17)
 
+print(board.D25)
+
 # Define the onboard LED
-LED = digitalio.DigitalInOut(board.D13)
-LED.direction = digitalio.Direction.OUTPUT
+# LED = digitalio.DigitalInOut(board.D13)
+# LED.direction = digitalio.Direction.OUTPUT
 
 # Initialize SPI bus.
 spi = busio.SPI(board.D11, MOSI=board.D10, MISO=board.D9)
@@ -37,7 +39,7 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, # must specify para
 
 # You can however adjust the transmit power (in dB).  The default is 13 dB but
 # high power radios like the RFM95 can go up to 23 dB:
-rfm9x.tx_power = 14
+rfm9x.tx_power = 23
 
 # Send a packet.  Note you can only send a packet up to 252 bytes in length.
 # This is a limitation of the radio packet size, so if you need to send larger
@@ -53,18 +55,22 @@ print("Sent Hello World message!")
 print("Waiting for packets...")
 
 while True:
-    packet = rfm9x.receive()
+    packet = rfm9x.receive(
+        keep_listening=True,
+        # timeout=10.0,
+        # with_header=True
+        )
     # Optionally change the receive timeout from its default of 0.5 seconds:
     # packet = rfm9x.receive(timeout=5.0)
     # If no packet was received during the timeout then None is returned.
     if packet is None:
         # Packet has not been received
-        LED.value = False
+        # LED.value = False
         now = datetime.now()
         print(now.strftime("%H:%M:%S"), "|", "Received nothing! Listening again...")
     else:
         # Received a packet!
-        LED.value = True
+        # LED.value = True
         # Print out the raw bytes of the packet:
         print("Received (raw bytes): {0}".format(packet))
         # And decode to ASCII text and print it too.  Note that you always
