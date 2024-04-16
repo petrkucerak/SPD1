476c3e8dc8b15c7fb249bf54f474b4d5d74be569
Petr Kucera <petr@khome.cz>
2024-04-16 18:20:16 +0200

    Create simple server

---
+++ gateway/lora_server.py
@@ -0,0 +1,21 @@
+from pyLoraRFM9x import LoRa, ModemConfig
+
+# This is our callback function that runs when a message is received
+def on_recv(payload):
+    print("From:", payload.header_from)
+    print("Received:", payload.message)
+    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+
+# Lora object will use spi port 0 and use chip select 1. GPIO pin 5 will be used for interrupts and set reset pin to 25
+# The address of this device will be set to 2
+lora = LoRa(0, 1, 5, 2, reset_pin = 25, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
+lora.on_recv = on_recv
+
+# Send a message to a recipient device with address 10
+# Retry sending the message twice if we don't get an  acknowledgment from the recipient
+message = "Hello there!"
+status = lora.send_to_wait(message, 10, retries=2)
+if status is True:
+    print("Message sent!")
+else:
+    print("No acknowledgment from recipient")
