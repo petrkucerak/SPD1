fc6260c5374ca69149bbab673b12fb05539fb4c3
petrkucerak <petr@khome.cz>
2024-05-01 11:02:59 +0200

    Remove map implementation

--- server/app/stations/page.js
+++ server/app/stations/page.js
@@ -1,11 +1,8 @@
-"use client";
-import MapComponent from "@/components/map";
-
 export default function Stations() {
   return (
     <div className="w-full">
       <h1>Stanice</h1>
-      <MapComponent />
+      
     </div>
   );
 }
