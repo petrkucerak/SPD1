7fdde2354cccc6b774e5b36cada87fe6ca90ea70
petrkucerak <petr@khome.cz>
2024-05-14 20:44:00 +0200

    Implement filtering by params

--- server/app/api/data/route.js
+++ server/app/api/data/route.js
@@ -1,7 +1,12 @@
 import fs from "fs";
 
-export async function GET() {
+export async function GET(request) {
   // load data
+  const { searchParams } = new URL(request.url);
+  const GN = searchParams.get("GN");
+  const GE = searchParams.get("GE");
+  console.log(GN, GE);
+
   const path = "../logs";
   const files = fs.readdirSync(path).sort();
   let raw_data = [];
@@ -16,7 +21,12 @@ export async function GET() {
   let data = [];
   for (let i = 0; i < raw_data.length; i += 1) {
     if (raw_data[i].GN === undefined || raw_data[i].GE === undefined) continue;
-    else
+    else {
+      // filter data by params
+      if (GN !== null && GE !== null) {
+        // if is not same as GN and GE skip it
+        if (raw_data[i].GN !== GN || raw_data[i].GE !== GE) continue;
+      }
       data.push({
         Tw: parseFloat(raw_data[i].Tw),
         Ta: parseFloat(raw_data[i].Ta),
@@ -26,6 +36,7 @@ export async function GET() {
         GE: parseFloat(raw_data[i].GE),
         time: raw_data[i].time,
       });
+    }
   }
   return Response.json({ data });
 }
